0bf639ba00093b51e2c451958c7b66bb196816a6
Brian Miranda <brianmiranda@outlook.com>
2018-07-24 16:42:05 -0500

    Query the Last.fm API

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,11 +1,12 @@
 var Trackster = {};
-
+const API_KEY = '2bbe2722514f807faf8d22e800a914aa';
 
 $(document).ready(function() {
-  $('#search-button').on('click', function() {
-  Trackster.searchTracksByTitle($("#search-input").val())
+  $('#search-button').click(function() {
+    Trackster.searchTracksByTitle($('#search-input').val());
+  });
 });
-});
+
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
@@ -18,6 +19,24 @@ Trackster.renderTracks = function(tracks) {
   Given a search term as a string, query the LastFM API.
   Render the tracks given in the API query response.
 */
-Trackster.searchTracksByTitle = function(title) {
 
+
+
+
+
+
+
+
+
+
+
+
+
+Trackster.searchTracksByTitle = function(title) {
+  $.ajax({
+    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    success: function(request) {
+      console.log(title);
+    }
+  });
 };
