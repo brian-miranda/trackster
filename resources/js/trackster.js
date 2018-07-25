0c7079be6f55e95946ff85dbc384f6f8163f76fe
Brian Miranda <brianmiranda@outlook.com>
2018-07-25 12:30:21 -0500

    Final version

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
   $('#search-button').click(function() {
     Trackster.searchTracksByTitle($('#search-input').val());
   });
+  
+  $('#search-input').keydown(function(event) {
+    var key = event.which;
+    if(key === 13){
+      Trackster.searchTracksByTitle($('#search-input').val());
+    }
+  });
 });
 
 /*
@@ -13,7 +20,27 @@ $(document).ready(function() {
 */
 
 Trackster.renderTracks = function(tracks) {
-
+  $("h1").removeClass("logo");
+  var $trackList = $('#track-list');
+  $trackList.empty();
+  for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+    var track = tracks[trackIndex];
+    var mediumAlbumArt = track.image[1]["#text"];
+    var listeners = numeral(track.listeners).format('0,0');
+    var htmlTrackRow =
+      '<div class="row track">' +
+      '  <div class="col-xs-1 col-xs-offset-1 play-button">' +
+      '    <a href="'+ track.url + '" target="_blank">' +
+      '      <i class="fa fa-play-circle-o fa-2x"></i>' +
+      '    </a>' +
+      '  </div>' +
+      '  <div class="col-xs-4">' + track.name + '</div>' +
+      '  <div class="col-xs-2">' + track.artist + '</div>' +
+      '  <div class="col-xs-2"><img src="' + mediumAlbumArt + '"/></div>' +
+      '  <div class="col-xs-2">' + listeners + '</div>' +
+      '</div>';
+    $trackList.append(htmlTrackRow);
+  }
 };
 
 /*
@@ -22,10 +49,11 @@ Trackster.renderTracks = function(tracks) {
 */
 
 Trackster.searchTracksByTitle = function(title) {
+  $("h1").addClass("logo");
   $.ajax({
-    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
     success: function(response) {
-      console.log(title);
+      Trackster.renderTracks(response.results.trackmatches.track);
     }
   });
 };
